b9473179946d5f9cc1c26faf67953bb6bc8ff8f0
Udayvscode <uk009uk27@gmail.com>
2026-04-24 15:31:28 +0530

    feat: add subscription model for user channel subscriptions

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from 'mongoose';
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, //one who is subcribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom to 'subscriber' is subcribing
+        ref: "User"
+    }
+}, {timestamps: true})
+
+export const Subscription = mongoose.model('Subscription', subscriptionSchema);
